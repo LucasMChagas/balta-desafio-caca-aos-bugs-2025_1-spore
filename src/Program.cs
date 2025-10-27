59b577541f5083db30f812451b149d5e0659800e
Lucas Miranda <lucas_miranda_chagas@hotmail.com>
2025-10-27 12:22:41 -0300

    Creation of migrations

--- src/Program.cs
+++ src/Program.cs
@@ -1,7 +1,15 @@
+using BugStore.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 string connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? String.Empty;
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlite(connectionString);
+});
 
 app.MapGet("/", () => "Hello World!");
 
